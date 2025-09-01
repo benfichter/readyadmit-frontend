3c0a44da0f4bde58235f79822851e0670ff9453e
yaksipper114 <haha53raid@gmail.com>
2025-08-31 23:37:38 -0400

    Updates for deployment mode

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": { target: "http://localhost:3000", changeOrigin: true }
+      "/api": { target: "https://readyadmit-backend-production.up.railway.app", changeOrigin: true }
     }
   }
 });
